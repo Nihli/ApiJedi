c54a26c35aff0ad5bbff800df98bd44b313f9856
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-05-21 14:57:33 -0300

    Finalizado Controller de Jedi

--- Controllers/JediController.cs
+++ Controllers/JediController.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using API_StarWars.Data;
+using API_StarWars.Models;
 
 namespace API_StarWars.Controllers
 {
@@ -7,10 +12,77 @@ namespace API_StarWars.Controllers
     public class JediController : ControllerBase
     {
         [HttpGet]
-        [Route("")]
-        public ActionResult<string> HelloWorld()
+        [Route("{id}")]
+        public async Task<ActionResult<Jedi>> GetJedi(
+            [FromServices] DataContext dataContext,
+            int id
+        )
         {
-            return Ok("Hello World!");
+            Jedi jedi = await dataContext.JediSet.FirstOrDefaultAsync(x => x.Id == id);
+            if(jedi != null)
+            {
+                return Ok(jedi);
+            }
+            return NotFound("Jedi não encontrado!");
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Jedi>> CreateJedi(
+            [FromServices] DataContext dataContext,
+            [FromBody] Jedi jedi
+        )
+        {
+            dataContext.JediSet.Add(jedi);
+            await dataContext.SaveChangesAsync();
+            return Ok(jedi);
+        }
+
+        [HttpGet]
+        [Route("jediNoPlural")]
+        public async Task<ActionResult<List<Jedi>>> GetJediNoPlural(
+            [FromServices] DataContext dataContext
+        )
+        {
+            List<Jedi> jediNoPlural = await dataContext.JediSet.ToListAsync();
+            return Ok(jediNoPlural);
+        }
+
+        [HttpDelete]
+        [Route("remove/{id}")]
+        public async Task<ActionResult<string>> RemoveJedi([FromServices] DataContext dataContext, int id)
+        {
+            Jedi jedi = await dataContext.JediSet.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (jedi == null)
+            {
+                return BadRequest("Jedi não encontrado.");
+            }
+
+            dataContext.JediSet.Remove(jedi);
+            await dataContext.SaveChangesAsync();
+
+            return Ok("Jedi eliminado.");
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<ActionResult<Jedi>> UpdateJedi(
+            [FromServices] DataContext dataContext,
+            [FromBody] Jedi jedi
+        )
+        {
+            Jedi jedi2 = await dataContext.JediSet.FirstOrDefaultAsync(x => x.Id == jedi.Id);
+
+            if (jedi2 == null)
+            {
+                return NotFound("Jedi não encontrado.");
+            }
+
+            dataContext.Entry(jedi2).CurrentValues.SetValues(jedi);
+            await dataContext.SaveChangesAsync();
+
+            return Ok(jedi);
         }
     }
 }
--- Models/Jedi.cs
+++ Models/Jedi.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API_StarWars.Models
 {
     public class Jedi
     {
+        [Key]
+        public long Id { get; set; }
         public string Nome { get; set; }
         public string Titulo { get; set; }
         public int Idade { get; set; }
